7d5570c29fb39441939b71f1333bfa8a90295d42
Messiah <m.muzafarov@gmail.com>
2013-04-23 10:54:53 +0600

    hmmm

--- Part2/Lesson5/HomeWork/bsqrt.cpp
+++ Part2/Lesson5/HomeWork/bsqrt.cpp
@@ -13,37 +13,46 @@ int main(int argc, const char* argv[])
     IRBuilder<> builder(getGlobalContext());
     Module* theModule = new Module("module.1", getGlobalContext());
 
-    std::vector<Type*> params(2, Type::getInt32Ty(getGlobalContext()));
+    std::vector<Type*> params(0, Type::getInt32Ty(getGlobalContext()));
     FunctionType* functionType =
         FunctionType::get(Type::getInt32Ty(getGlobalContext()), params, false);
     Function* function = Function::Create(functionType,
                                           Function::ExternalLinkage, "main",
                                           theModule);
-    Function::arg_iterator args = function->arg_begin();
+    std::vector<Type*> param2(1, Type::getDoubleTy(getGlobalContext()));
+    FunctionType* functionType2 =
+        FunctionType::get(Type::getDoubleTy(getGlobalContext()), param2, false);
+    Function* function2 = Function::Create(functionType,
+                                          Function::ExternalLinkage, "bsqrt",
+                                          theModule);
+    Function::arg_iterator args = function2->arg_begin();
     Value* x = args++;
     x->setName("x");
-    Value* y = args++;
-    y->setName("y");
 
-    BasicBlock* entry = BasicBlock::Create(getGlobalContext(), "bb",
+    BasicBlock* entry = BasicBlock::Create(getGlobalContext(), "enter_main",
                                            function);
-    BasicBlock* ret = BasicBlock::Create(getGlobalContext(), "bb",
+    BasicBlock* entry_bsqrt = BasicBlock::Create(getGlobalContext(), "enter_bsqrt",
+                                           function2);
+    BasicBlock* ret = BasicBlock::Create(getGlobalContext(), "return_main",
                                          function);
+    BasicBlock* ret2 = BasicBlock::Create(getGlobalContext(), "return_bsqrt",
+                                         function2);
     BasicBlock* cond_false = BasicBlock::Create(getGlobalContext(),
-                                                "bb", function);
+                                                "if_false", function2);
     BasicBlock* cond_true = BasicBlock::Create(getGlobalContext(), "cond_true",
-                                               function);
+                                               function2);
     BasicBlock* cond_false_2 = BasicBlock::Create(getGlobalContext(),
-                                                  "cond_false2", function);
+                                                  "cond_false2", function2);
+    BasicBlock* display = BasicBlock::Create(getGlobalContext(), "print", function);
 
     builder.SetInsertPoint(entry);
-    Value* xEqualsY = builder.CreateICmpEQ(x, y, "tmp");
-    builder.CreateCondBr(xEqualsY, ret, cond_false);
-
-    builder.SetInsertPoint(ret);
-    builder.CreateRet(x);
-
-    builder.SetInsertPoint(cond_false);
+    builder.CreateRet(0);
+    builder.SetInsertPoint(display);
+    std::vector<Value*> args1;
+    args1.push_back(x);
+    Value* bi_sqrt = builder.CreateCall(function, args1, "bin_bsqrt");
+    builder.CreateRet(bi_sqrt);
+    /*builder.SetInsertPoint(cond_false);
     Value* xLessThanY = builder.CreateICmpULT(x, y, "tmp");
     builder.CreateCondBr(xLessThanY, cond_true, cond_false_2);
 
@@ -62,9 +71,10 @@ int main(int argc, const char* argv[])
     args2.push_back(y);
     Value* recur_2 = builder.CreateCall(function, args2, "tmp");
     builder.CreateRet(recur_2);
-
+    */
     verifyFunction(*function);
-
+    
+    
     theModule->dump();
 
     std::string errorString;
